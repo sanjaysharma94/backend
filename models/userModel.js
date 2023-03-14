1bd8710f0d4d46fb67be02520d6d6607c38f88a3
Sanjay Sharma <forusanjay007@gmail.com>
2023-03-14 15:26:28 +0530

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -1,20 +1,13 @@
 
 const mongoose = require("mongoose");
-
-
-
 const userSchema = new mongoose.Schema({
-   
-       
         firstname:{type : String, },
         lastname: {type : String, },
         photo:{type : String, },
         age: {type : Number, },
         gender: {type : String, },
-        phone: {type : Number, },
-    
+        phone: {type : Number, }, 
 })
-
 
 const User = new mongoose.model("User", userSchema)
 
