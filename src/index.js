e124dc581ae2e4e2bc311ff3f6e5ba37b6171590
Sanjay Sharma <forusanjay007@gmail.com>
2023-03-14 11:32:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const mongoose = require("mongoose");
-const userRoutes = require("./Routes/userRoute")
+const userRoutes = require("../Routes/userRoute")
 
 const app = express();
 app.use(express.json());
